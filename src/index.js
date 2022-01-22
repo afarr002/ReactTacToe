2af76e894d19c9b98779d1f19823dafd1abb1856
aaron <afarrell002@gmail.com>
2022-01-22 16:39:49 -0700

    removed Square from index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const Square = ({ onClick, value }) => {
-  return (
-    <button className="square" onClick={onClick}>
-      {value}
-    </button>
-  );
-};
-
 class Board extends React.Component {
   renderSquare(i) {
     return (
